89e9bdbe1c6b97b3f1eea227085442de8c3486fb
Nuno Gonçalves <nuno.goncalves@linkedcare.com>
2014-08-09 15:01:46 +0100

    ok. animation looks a bit better

--- src/com/numicago/android/gasolina/activities/viewloaders/StationsListViewLoader.java
+++ src/com/numicago/android/gasolina/activities/viewloaders/StationsListViewLoader.java
@@ -3,7 +3,9 @@ package com.numicago.android.gasolina.activities.viewloaders;
 import java.util.List;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.view.Gravity;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
@@ -14,6 +16,7 @@ import com.numicago.android.gasolina.objects.Station;
 public class StationsListViewLoader extends ViewLoader {
 
 	private ListView lv;
+	private ImageView tweenImage;
 	private LinearLayout gpsGifImageViewLayout; 
 
 	public StationsListViewLoader(Activity activity) {
@@ -24,10 +27,14 @@ public class StationsListViewLoader extends ViewLoader {
 	@Override
 	protected void instantiateViews() {
 		gpsGifImageViewLayout = (LinearLayout) activity.findViewById(R.id.stationsListGPSLookupLL);
+		tweenImage = (ImageView) activity.findViewById(R.id.stationsListGpsAnimationImage);
 	}
 
 	@Override
 	protected void fillViews() {
+		tweenImage.setBackgroundResource(R.anim.gps_animation);
+		AnimationDrawable frameAnimation = (AnimationDrawable) tweenImage.getBackground();
+		frameAnimation.start();
 	}
 
 	public void loadStationsList(List<Station> stations) {
